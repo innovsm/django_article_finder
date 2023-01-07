d3c61523f9f048102eed7f9506981f6610682ab2
SHUBHANSHU MISHRA <81454420+innovsm@users.noreply.github.com>
2023-01-07 12:22:25 +0530

    Update urls.py

--- article_finder/urls.py
+++ article_finder/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from finder.views import HomePageView,alfa_request,hello_world
+from finder.views import HomePageView,alfa_request,hello_world,advanced,manage_adv
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomePageView.as_view()),
-    path("alfa",hello_world, name ="test_alfa")
+    path("alfa",hello_world, name ="test_alfa"),
+    # --------------- advanced search -------------------------
+    path("adv",advanced.as_view()),
+    path("alfa_adv",manage_adv)
 
 ]
